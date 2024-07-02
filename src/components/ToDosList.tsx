c0c0d0751e0b737ea6af4136c0530a49a097e89c
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-07-02 14:55:51 +0300

    Implement the ability to delete To-Do items from the list

--- src/components/ToDosList.tsx
+++ src/components/ToDosList.tsx
@@ -1,3 +1,4 @@
+import DeleteIcon from "@mui/icons-material/Delete";
 import DoneIcon from "@mui/icons-material/Done";
 import RedoIcon from "@mui/icons-material/Redo";
 import {
@@ -7,6 +8,7 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Typography,
 } from "@mui/material";
 import React from "react";
 import ToDo from "../entities/ToDo";
@@ -24,11 +26,33 @@ const ToDosList = ({ currentTodos, setCurrentTodos }: Props) => {
     setCurrentTodos(updatedTodos);
   };
 
+  const handleDelete = (id: number) => {
+    const filteredTodos = currentTodos.filter((todo) => todo.id !== id);
+    setCurrentTodos(filteredTodos);
+  };
+
+  if (currentTodos.length === 0)
+    return (
+      <Typography variant="h6" mt={3}>
+        No To-Do items available!
+      </Typography>
+    );
+
   return (
     <List>
       {currentTodos.map((todo) => (
         <React.Fragment key={todo.id}>
-          <ListItem>
+          <ListItem
+            secondaryAction={
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => handleDelete(todo.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
             <ListItemAvatar>
               <IconButton
                 disableRipple
